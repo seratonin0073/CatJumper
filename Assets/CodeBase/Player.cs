2c3fe9a65e640598eb9207c0008d980238baccd2
seratonin0073 <mizko2000y@gmail.com>
2023-10-24 20:46:50 +0300

    FinishCommit

---
+++ Assets/CodeBase/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    private float moveDirection;
+    private Rigidbody2D plaeyRB;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
