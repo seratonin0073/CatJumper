42efb587ff46c79b11340d48aaad35b2eb12ca43
seratonin0073 <mizko2000y@gmail.com>
2023-10-28 18:51:47 +0300

    Lesson2.1

--- Assets/CodeBase/CameraMove.cs
+++ Assets/CodeBase/CameraMove.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 100f;//швидкість камери
+
+    private Transform target;//об'єкт за яким буде камера слідкувати 
     void Start()
     {
-        
+        target = FindObjectOfType<Player>().transform;//шукаємо таргет за скриптом Player
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target.position.y > transform.position.y)//якщо таргет вище за камеру
+        {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, -10);//створюємо те куди камера буде рухатись
+            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * speed);//рухаємо камеру в новову позицію
+        }
     }
 }
